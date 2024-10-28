31b65dc4288f4f2943552077bf7063fcf745b877
Christian_Yemele <yemelechristian2@gmail.com>
2024-10-28 17:09:48 +0100

    feat(): didcomm form

--- clients/src/App.tsx
+++ clients/src/App.tsx
@@ -1,35 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useFormik } from 'formik';
+import Mediation_Coordinaton from './utils/mediation';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function SignupForm() {
+  // Note that we have to initialize ALL of fields with values. These
+  // could come from props, but since we don’t want to prefill this form,
+  // we just use an empty string. If we don’t do this, React will yell
+  // at us.
+  const formik = useFormik({
+    initialValues: {
+      recipient_did: '',
+      mediator_did: [''],
+      action: '',
+    },
+    onSubmit: values => {
+     Mediation_Coordinaton(values.mediator_did, values.recipient_did, values.action)
+    },
+  });
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <form onSubmit={formik.handleSubmit}>
+      <label htmlFor="recipient_did">Recipient_DID</label>
+      <input
+        id="recipient_did"
+        name="recipient_did"
+        type="text"
+        onChange={formik.handleChange}
+        value={formik.values.recipient_did}
+      />
 
-export default App
+      <label htmlFor="Mediator_did">Mediator_DID</label>
+      <input
+        id="mediator_did"
+        name="mediator_did"
+        type="text"
+        onChange={formik.handleChange}
+        value={formik.values.mediator_did}
+      />
+
+      <label htmlFor="action">Action</label>
+      <input
+        id="action"
+        name="action"
+        type="action"
+        onChange={formik.handleChange}
+        value={formik.values.action}
+      />
+
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
