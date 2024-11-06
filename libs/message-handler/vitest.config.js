306a7a44878bb1f7a33d4bfba0c3217ab0eb479a
Christian_Yemele <yemelechristian2@gmail.com>
2024-11-06 11:31:25 +0100

    fix(): added config for didcomm test intergration

---
+++ libs/message-handler/vitest.config.js
@@ -0,0 +1,14 @@
+/// <reference types="vitest/config" />
+import wasm from 'vite-plugin-wasm';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  plugins: [wasm()],
+  test: {
+    server: {
+      deps: {
+        inline: [/\.wasm$/],
+      },
+    },
+  },
+});
